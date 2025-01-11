84eb8790c6743feecd967d915436a81f76e30371
Vincent St-Pierre <vstpierre1@gmail.com>
2025-01-11 14:13:52 -0500

    Tile + GameManager commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static int lifePoints;
+    public static int coins;
+    public static int timeLeft;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,18 +6,7 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    public bool isRevealed
-    {
-        get
-        {
-            return isRevealed;
-        }
-        set
-        {
-            isRevealed = value;
-            ChangeSprite();
-        }
-    }
+    public bool isRevealed;
 
     public int dangerLevel
     {
@@ -46,8 +35,8 @@ public class Tile : MonoBehaviour
         }
     }
 
-    public Sprite _dangerLevelSprite;
-    public Sprite _tileContentSprite;
+    public Sprite dangerLevelSprite;
+    public Sprite tileContentSprite;
     public SpriteRenderer dangerLevelRenderer;
     public SpriteRenderer tileContentRenderer;
 
@@ -69,9 +58,75 @@ public class Tile : MonoBehaviour
         this.tileContent = tileContent;
     }
 
+    // If isClicked true, then player clicked on the tile, else tile was revealed by lighthouse.
+    public void RevealTile(bool isClicked)
+    {
+        isRevealed = true;
+        ChangeSprite();
+        switch (tileContent) {
+            case TileContent.Shark:
+                SharkBite();
+                break;
+            case TileContent.TreasureSmall:
+                GameManager.coins += 10;
+                treasureObtained();
+                break;
+            case TileContent.TreasureMedium:
+                GameManager.coins += 20;
+                treasureObtained();
+                break;
+            case TileContent.TreasureLarge:
+                GameManager.coins += 50;
+                treasureObtained();
+                break;
+        }
+    }
+
+    private void treasureObtained()
+    {
+        
+    }
+
+    // Player loses a life from revealing a shark
+    private void SharkBite()
+    {
+        GameManager.lifePoints--;
+    }
 
     private void ChangeSprite()
     {
-        throw new NotImplementedException();
+        ChangeSpriteWater();
     }
+
+    private void ChangeSpriteWater()
+    {
+        if (isRevealed)
+        {
+            // Clear water
+            if (dangerLevel == 0)
+            {
+                dangerLevelRenderer.sprite = Resources.Load<Sprite>("Environnement/Prefab/shade_0");
+            }
+            // 1-2 Sharks
+            else if (dangerLevel > 0 && dangerLevel < 3) 
+            {
+                dangerLevelRenderer.sprite = Resources.Load<Sprite>("Environnement/Prefab/shade_1");
+            }
+            // 3-4 sharkss
+            else if (dangerLevel > 2 && dangerLevel < 5)
+            {
+                dangerLevelRenderer.sprite = Resources.Load<Sprite>("Environnement/Prefab/shade_2");
+            }
+            // 5+ sharks
+            else
+            {
+                dangerLevelRenderer.sprite = Resources.Load<Sprite>("Environnement/Prefab/shade_3");
+            }
+        }
+        else
+        {
+            dangerLevelRenderer.sprite = Resources.Load<Sprite>("Environnement/Prefab/shade_4");
+        }
+    }
+
 }
