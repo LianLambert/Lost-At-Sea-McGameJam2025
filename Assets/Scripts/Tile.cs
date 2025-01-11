c364958712786fa369cea52df69f2865952485c0
Vincent St-Pierre <vstpierre1@gmail.com>
2025-01-11 11:11:27 -0500

    Tile constructor + layers

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,20 +8,31 @@ public class Tile : MonoBehaviour
     public bool isRevealed;
     public int dangerLevel;
     public TileContent tileContent;
-    [SerializeField] Sprite currentSprite;
-    [SerializeField] SpriteRenderer currentRenderer;
-    [SerializeField] Animation tileAnimation;
+    [SerializeField] Sprite[] dangerLevelSpriteArray;
+    [SerializeField] Sprite tileContentSprite;
+    public SpriteRenderer dangerLevelRenderer;
+    public SpriteRenderer tileContentRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    public Tile(bool isRevealed, int dangerLevel, TileContent tileContent){
+        this.isRevealed = isRevealed;
+        this.dangerLevel = dangerLevel;
+        this.tileContent = tileContent;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ChangeSprite();
     }
 
+    private void ChangeSprite()
+    {
+        
+    }
 }
